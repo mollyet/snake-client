c5c58923e2141b54b8e2555531e5a31e99c1ef5d
Molly Trepanier <mtrepanier123@gmail.com>
2021-02-10 22:10:34 +0000

    made client.js to handle server connection, and exported server cxn file over play.js

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require('net');
+/**
+ * 
+ * Sets up cxn to lhl server
+ * swap to host: 'localhost' and port: '8001' for running your own server 
+ * (also clone this: https://github.com/lighthouse-labs/snek-multiplayer && run npm install && npm run play to set server up)
+ * 
+ */
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+  });
+  // interprets incoming data as text
+  conn.setEncoding('utf8');
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  return conn;
+};
+
+//
+
+module.exports = { connect } ;
--- play.js
+++ play.js
@@ -1,26 +1,7 @@
 
-const net = require('net')
+const net = require('net');
 
-/**
- * 
- * Sets up cxn to lhl server
- * swap to host: 'localhost' and port: '8001' for running your own server 
- * (also clone this: https://github.com/lighthouse-labs/snek-multiplayer && run npm install && npm run play to set server up)
- * 
- */
+const { connect } = require('./client');
 
- const connect = function() {
-   const conn = net.createConnection({
-     host: '135.23.222.131',
-     port: 50542
-   });
-   // interprets incoming data as text
-   conn.setEncoding('utf8');
-   conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-   return conn;
- };
-
- console.log("Connecting . . . ");
- connect();
+console.log("Connecting . . . ");
+connect();
