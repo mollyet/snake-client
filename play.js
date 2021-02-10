d84f8134b7eaaec879c1397e74aa84858be98604
Molly Trepanier <mtrepanier123@gmail.com>
2021-02-10 23:32:10 +0000

    added setUpInput and handleUserInput f(x)s that utlize process.stdin

--- play.js
+++ play.js
@@ -1,7 +1,30 @@
 
 const net = require('net');
-
 const { connect } = require('./client');
-
 console.log("Connecting . . . ");
 connect();
+
+const setUpInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+};
+setUpInput();
+
+/**
+ * Event Handling for User Input
+ */
+
+
+const handleUserInput = function (data) {
+  const stdin = process.stdin;
+  stdin.on("data", (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+};
+handleUserInput();
+
